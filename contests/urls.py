54444e1fa9ffc7169709ab3962349e2734318307
Roopak Chugh <chughroopak@gmail.com>
2018-04-17 10:34:43 +0530

    Project Update

--- contests/urls.py
+++ contests/urls.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
 
-@author: Saurav Kanchan
-
-"""
 from django.conf.urls import url,include
 from .views import *
 urlpatterns = [
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
 
-@author: Saurav Kanchan
-
-"""
 from django.conf.urls import url,include
 from .views import *
 from django.contrib.auth import views as auth_views
@@ -12,6 +8,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url('accounts/profiles/(?P<id>\d+)',profiles,name='general_profile'),
     url('accounts/profile/',profile,name='profile'),
-    url('^$',home,name='home'),
+    url('^$', home,name='home'),
 
 ]
